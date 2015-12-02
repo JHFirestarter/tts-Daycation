454c4c3c33545189d8033bce624f346c97636e60
Jim Hardin <jim@vibaso.com>
2015-12-02 11:18:16 -0500

    cleaned up params etc

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,10 @@
 class Destination < ActiveRecord::Base
 	geocoded_by :address
 	after_validation :geocode
+
+	def random
+		# whatever
+		# on click of home page button, randomly redirect (whole link) to a destination show page
+	end
+
 end
